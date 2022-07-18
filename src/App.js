b81430c0f333120316467437029d24dd3beea2b4
michael <michaelsanchez8815@gmail.com>
2022-07-18 15:55:00 -0500

    Apply eslinter/prettier rules

--- src/App.js
+++ src/App.js
@@ -1,28 +1,26 @@
+import React from 'react';
 import { ThemeProvider } from 'styled-components';
-import theme from './theme';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import theme from './theme';
 import Hero from './features/Hero';
 import Student from './features/Student';
 import Adult from './features/Adult';
 import AdultLogin from './features/Adult/login';
 import StudentLogin from './features/Student/login';
 
-
-function App() {
-  return (
-    <ThemeProvider theme={theme}>
-      {/* test */}
-      <Router>
-        <Routes>
-          <Route path='/' element={<Hero />} exact />
-          <Route path='/student' element={<Student />} exact />
-          <Route path='/student/login' element={<StudentLogin />} exact />
-          <Route path='/adult' element={<Adult />} exact />
-          <Route path='/adult/login' element={<AdultLogin />} exact />
-        </Routes>
-      </Router>
-    </ThemeProvider>
-  );
-}
+const App = () => (
+  <ThemeProvider theme={theme}>
+    {/* test */}
+    <Router>
+      <Routes>
+        <Route path="/" element={<Hero />} exact />
+        <Route path="/student" element={<Student />} exact />
+        <Route path="/student/login" element={<StudentLogin />} exact />
+        <Route path="/adult" element={<Adult />} exact />
+        <Route path="/adult/login" element={<AdultLogin />} exact />
+      </Routes>
+    </Router>
+  </ThemeProvider>
+);
 
 export default App;
--- src/features/Hero/index.jsx
+++ src/features/Hero/index.jsx
@@ -2,7 +2,15 @@ import React from 'react'
 
 const Hero = () => {
   return (
-    <div>Hero</div>
+    <div style={{ height: '100vh', width: '100vw', display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'column' }}>
+      <div>ALIEN BUDGETS</div>
+      <div>WILL YOU GO BROKE OR WILL YOU BE SUCCESSFUL?</div>
+      <div>Who are you</div>
+      <div>
+        <button>Student</button>
+        <button>Adult</button>
+      </div>
+    </div>
   )
 }
 
