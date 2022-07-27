13f55c2226eb9b83732e6166cf7da2215141c6c4
michael <michaelsanchez8815@gmail.com>
2022-07-27 10:01:33 -0500

    Create part of Register Student

---
+++ src/constants/endpoints.js
@@ -0,0 +1,6 @@
+/* eslint-disable no-unused-vars */
+const BASE_URL = process.env.BACKEND_API_ENDPOINT;
+const URL_AUTH = `${BASE_URL}/auth`;
+const URL_STUDENT = `${BASE_URL}/students`;
+const URL_ADULT = `${BASE_URL}/adults`;
+const URL_AVATAR = `${BASE_URL}/avatar`;
--- src/features/Student/Register/index.jsx
+++ src/features/Student/Register/index.jsx
@@ -1,5 +1,111 @@
 import React from 'react';
+import { Form, Input } from 'antd';
+import StyledTitle from '../../../components/Title';
 
-const RegisterStudent = () => <div>RegisterStudent</div>;
+const RegisterStudent = () => {
+  const [form] = Form.useForm();
+
+  const onFinish = (values) => {
+    console.log('Received values of form: ', values);
+  };
+
+  const passwordRegex =
+    /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[-#$^+_!*()@%&]).{8,20}$/gm;
+
+  return (
+    <>
+      <StyledTitle>NEW ALIEN</StyledTitle>
+      <Form form={form} name="registerStudent" onFinish={onFinish}>
+        <Form.Item
+          name="firstName"
+          hasFeedback
+          rules={[
+            {
+              type: 'text',
+              required: true,
+              message: 'Please input your first name.',
+            },
+            {
+              pattern: /[a-zA-Z]{3,}/gm,
+              message: 'Must be minimum 3 letters.',
+            },
+          ]}
+        >
+          <Input type="text" placeholder="First Name" />
+        </Form.Item>
+        <Form.Item
+          name="lastInitial"
+          hasFeedback
+          rules={[
+            {
+              type: 'text',
+              required: true,
+              message: 'Please input your last initial.',
+            },
+            {
+              pattern: /^[a-zA-Z]{1}$/gm,
+              message: 'Please only write one letter.',
+            },
+          ]}
+        >
+          <Input type="text" placeholder="Last Initial" />
+        </Form.Item>
+        <Form.Item noStyle>
+          <div>
+            Password must be 8-20 characters, including: at least one capital
+            letter, at least one small letter, one number and one special
+            character - ! @ # $ % ^ & * ( ) _ +
+          </div>
+          <Form.Item
+            name="password"
+            hasFeedback
+            register="true"
+            rules={[
+              {
+                required: true,
+                message: 'Please input your Password!',
+              },
+              {
+                min: 8,
+                message: 'Must be a minimum of 8 characters',
+              },
+              {
+                pattern: passwordRegex,
+                message:
+                  'Password must be 8-20 characters, including: at least one capital letter, at least one small letter, one number and one special character - ! @ # $ % ^ & * ( ) _ +',
+              },
+            ]}
+          >
+            <Input type="password" placeholder="Password" />
+          </Form.Item>
+        </Form.Item>
+        <Form.Item
+          name="confirm"
+          dependencies={['password']}
+          hasFeedback
+          register="true"
+          rules={[
+            {
+              required: true,
+              message: 'Please confirm your password!',
+            },
+            ({ getFieldValue }) => ({
+              validator(_, value) {
+                if (!value || getFieldValue('password') === value) {
+                  return Promise.resolve();
+                }
+                return Promise.reject(
+                  new Error('The two passwords that you entered do not match!')
+                );
+              },
+            }),
+          ]}
+        >
+          <Input type="password" placeholder="Confirm Password" />
+        </Form.Item>
+      </Form>
+    </>
+  );
+};
 
 export default RegisterStudent;
