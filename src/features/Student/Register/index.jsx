9c71a8e66b22323d0df6f91f90ddf302da80f19c
michael <michaelsanchez8815@gmail.com>
2022-07-28 12:00:53 -0500

    Update register student with passsword and forgot

--- src/features/Student/Register/index.jsx
+++ src/features/Student/Register/index.jsx
@@ -1,9 +1,33 @@
 import React from 'react';
-import { Form, Input } from 'antd';
+import { Form, Input, Select } from 'antd';
 import StyledTitle from '../../../components/Title';
+import StyledButton from '../../../components/PrimaryButton';
 
 const RegisterStudent = () => {
   const [form] = Form.useForm();
+
+  const forgotPassQuestionCHange = (value) => {
+    switch (value) {
+      case 'Test1':
+        form.setFieldsValue({
+          note: 'Test1',
+        });
+        break;
+      case 'Test2':
+        form.setFieldsValue({
+          note: 'Test2',
+        });
+        break;
+      case 'Test3':
+        form.setFieldsValue({
+          note: 'Test3',
+        });
+        break;
+
+      default:
+        break;
+    }
+  };
 
   const onFinish = (values) => {
     console.log('Received values of form: ', values);
@@ -27,6 +51,7 @@ const RegisterStudent = () => {
             },
             {
               pattern: /[a-zA-Z]{3,}/gm,
+              required: true,
               message: 'Must be minimum 3 letters.',
             },
           ]}
@@ -44,6 +69,7 @@ const RegisterStudent = () => {
             },
             {
               pattern: /^[a-zA-Z]{1}$/gm,
+              required: true,
               message: 'Please only write one letter.',
             },
           ]}
@@ -67,16 +93,23 @@ const RegisterStudent = () => {
               },
               {
                 min: 8,
+                required: true,
                 message: 'Must be a minimum of 8 characters',
               },
               {
+                max: 20,
+                required: true,
+                message: 'Must be a maximum of 20 characters',
+              },
+              {
                 pattern: passwordRegex,
+                required: true,
                 message:
                   'Password must be 8-20 characters, including: at least one capital letter, at least one small letter, one number and one special character - ! @ # $ % ^ & * ( ) _ +',
               },
             ]}
           >
-            <Input type="password" placeholder="Password" />
+            <Input.Password type="password" placeholder="Password" />
           </Form.Item>
         </Form.Item>
         <Form.Item
@@ -101,7 +134,57 @@ const RegisterStudent = () => {
             }),
           ]}
         >
-          <Input type="password" placeholder="Confirm Password" />
+          <Input.Password type="password" placeholder="Confirm Password" />
+        </Form.Item>
+        <Form.Item
+          name="forgotPasswordQuestion"
+          rules={[
+            {
+              required: true,
+              message: 'Please select your question',
+            },
+          ]}
+        >
+          <Select
+            placeholder="Forgot Password Question"
+            onChange={forgotPassQuestionCHange}
+            allowClear
+          >
+            <Select.Option key="test1" value="test1">
+              Test1
+            </Select.Option>
+            <Select.Option key="test2" value="test2">
+              Test2
+            </Select.Option>
+            <Select.Option key="test3" value="test3">
+              Test3
+            </Select.Option>
+          </Select>
+        </Form.Item>
+        <Form.Item
+          name="forgotPasswordAnswer"
+          register="true"
+          rules={[
+            {
+              required: true,
+              message: 'Please write your answer.',
+            },
+          ]}
+        >
+          <Input type="text" placeholder="Forgot Password Answer" />
+        </Form.Item>
+        {/* Classroom Code */}
+        {/* Avatar and Username */}
+        <Form.Item register="true" style={{ textAlign: 'center' }}>
+          <div>By signing up you agree to our terms and policies.</div>
+          <StyledButton
+            larger="true"
+            type="primary"
+            htmlType="submit"
+            className="login-form-button"
+          >
+            Register
+          </StyledButton>
         </Form.Item>
       </Form>
     </>
